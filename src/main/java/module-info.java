f5a373eb05ff06943de8f00c15a818f2188f9123
acidicMercury8 <acidicmercury8@outlook.com>
2023-01-03 21:00:49 +0300

    Add empty JavaFX project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ru.imesense.javameemulator {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens ru.imesense.javameemulator to javafx.fxml;
+    exports ru.imesense.javameemulator;
+}
