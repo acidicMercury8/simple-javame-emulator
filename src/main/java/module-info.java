aa1ab03af428f1507bd77d52dcfbdda99cc9292d
acidicMercury8 <acidicmercury8@outlook.com>
2023-01-03 21:29:03 +0300

    Fix codestyle of Java files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module ru.imesense.javameemulator {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens ru.imesense.javameemulator to javafx.fxml;
     exports ru.imesense.javameemulator;
 }
