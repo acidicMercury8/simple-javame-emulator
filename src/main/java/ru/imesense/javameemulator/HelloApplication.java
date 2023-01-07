d032c3a6c39c26b7dffa7f03d4ec98426ee7efe5
acidicMercury8 <acidicmercury8@outlook.com>
2023-01-07 16:03:17 +0300

    Update title

--- src/main/java/ru/imesense/javameemulator/HelloApplication.java
+++ src/main/java/ru/imesense/javameemulator/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         var fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         var scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Java ME Emulator");
         stage.setScene(scene);
         stage.show();
     }
